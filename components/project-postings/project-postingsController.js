10500d5c838de49340e0758fe9ea465218ccf5f4
tjclavel <thomasclavelli@DN0a22a6f8.SUNet>
2017-05-01 14:00:06 -0700

    added job functionality and ability to delete jobs

--- components/project-postings/project-postingsController.js
+++ components/project-postings/project-postingsController.js
@@ -5,4 +5,16 @@ cs142App.controller('ProjectPostingsController', ['$scope',
       window.location.href = "#/add/project";
     };
 
+    $scope.pendingProject = {};
+
+    $scope.addingProject = function() {
+      $scope.projects.push($scope.pendingProject);
+      window.location.href = "#/posts";
+    };
+
+    $scope.removeProject = function(i) {
+      $scope.projects.splice(i, 1);
+      window.location.href = "#/posts";
+    };
+
   }]);
