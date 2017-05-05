c45b6bf214b71c42af85b3f9e67712cdfea7135a
tjclavel <thomasclavelli@DN51vl1q.SUNet>
2017-05-04 22:08:07 -0700

    added title icon

--- mainController.js
+++ mainController.js
@@ -30,7 +30,8 @@ cs142App.controller('MainController', ['$scope', '$resource',
     function ($scope, $resource) {
         $scope.main = {};
 
-        $scope.curId = 3;
+        $scope.curId = 4;
         $scope.projects = [{title: "doggo", image: "/images/doge.jpeg", id: 1},
-                            {title: "doge2", image: "/images/doge.jpeg", id: 2}];
+                           {title: "doggo treat", image: "/images/dog-treat.jpg", id: 2},
+                            {title: "gross furball thing", image: "/images/cat.jpg", id: 3}];
     }]);
